4d1beaaf9c067ceb9b6e9fdb205ed4fa13b2a239
mohd usman <usman12797@gmail.com>
2023-04-05 17:50:32 +0530

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+let box=document.getElementsByClassName('box')
+console.log(box)
+var ch=document.getElementById('Heading');
+function change(){
+ch.innerHTML="Hello World!!"
+}
+function replace(){
+   let rt=document.getElementById('rtext');
+   rt.innerHTML="Welcome to elevation academy"; 
+}
